06995dd1c0cf15e3c1b1900b4ed4694a32cbbb03
Chuan-Heng Hsiao <2970164+chhsiao1981@users.noreply.github.com>
2026-07-01 08:23:59 -0400

    for 16.0.1
    1. demo getStateByModule.
    2. demo getMod.

--- src/thunks/square.ts
+++ src/thunks/square.ts
@@ -1,7 +1,8 @@
 import {
   type State as _State,
   doMod,
-  getStateOrNullByModule,
+  getMod,
+  getStateByModule,
   type Thunk,
 } from "@chhsiao1981/use-thunk";
 import { ARRAY_9 } from "../const";
@@ -35,12 +36,14 @@ export const click = (
 
     const moduleState = getModuleState();
     const nextSquares = ARRAY_9.map((eachIdx) => {
-      const { value } = getStateOrNullByModule(
-        moduleState,
-        `${eachIdx}`,
-      ) as State; // XXX click only after all the squares are init.
+      const { value } = getStateByModule(moduleState, `${eachIdx}`);
       return value;
     });
+
+    const moduleState2 = getMod<State>(name);
+    if (moduleState !== moduleState2) {
+      console.error("sqaure.click (getMod): moduleState !== moduleState2");
+    }
 
     const doGame = doMod<gameState, TypeModGame>(gameName);
     doGame.play(nextSquares);
